e1294a9e8b1c1d85e4aafbce0d914824c8290423
Christian Miles <Christian.Miles@kier.co.uk>
2018-12-07 19:50:30 +0000

    Remove required attribute from middle initial validation

--- src/components/MainForm/MainForm.js
+++ src/components/MainForm/MainForm.js
@@ -80,7 +80,6 @@ export default Vue.extend({
                 required
             },
             middleInitial: {
-                required,
                 maxLength: maxLength(1)
             },
             floorAndRoom: {
