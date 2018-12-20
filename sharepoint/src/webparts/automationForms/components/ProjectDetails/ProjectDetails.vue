11cf39e123a86add46db5a36e99228c64586a6ad
Christian Miles <Christian.Miles@kier.co.uk>
2018-12-20 01:34:13 +0000

    Revert

--- sharepoint/src/webparts/automationForms/components/ProjectDetails/ProjectDetails.vue
+++ sharepoint/src/webparts/automationForms/components/ProjectDetails/ProjectDetails.vue
@@ -1,18 +1,18 @@
 <template>
     <div>
     <h2>Approval Information</h2>
-    <div class="form-group">
+    <!-- <div class="form-group">
         <label>Company*</label>
         <vue-select v-model="formData.companyName" :options="companies" :class="{ 'is-invalid': $v.formData.companyName.$invalid }"></vue-select>
     </div>
     <div class="form-group">
         <label>Business Unit*</label>
         <vue-select v-model="formData.businessUnit" :options="businessUnits" :class="{ 'is-invalid': $v.formData.businessUnit.$invalid }"></vue-select>
-    </div>
-    <div class="form-group">
+    </div> -->
+    <!-- <div class="form-group">
         <label>Division*</label>
         <vue-select v-model="formData.divisionName" :options="divisions" :class="{ 'is-invalid': $v.formData.divisionName.$invalid }"></vue-select>
-    </div>    
+    </div>     -->
     <div class="form-group">
         <label>Project Number*</label>
         <input type="text" class="form-control" v-model="formData.projectNumber">
