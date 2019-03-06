363ca48b68da0385d2f99ad0479ace096a4ed101
Gabor Vig <gaborvig@365dev.co.uk>
2019-03-06 15:09:06 +0000

    Merged PR 183: IE 11 bug has been resolved
    IE 11 bug has been resolved
    
    import 'core-js/es6/weak-map line commented out
    
    Related work items: #36960

--- src/webparts/automationForms/AutomationFormsWebPart.ts
+++ src/webparts/automationForms/AutomationFormsWebPart.ts
@@ -10,7 +10,7 @@ import 'core-js/es6/string';
 import 'core-js/es6/date';
 import 'core-js/es6/array';
 import 'core-js/es6/regexp';
-import 'core-js/es6/weak-map';
+// import 'core-js/es6/weak-map';
 import "@pnp/polyfill-ie11";
 // end polyfills
 
